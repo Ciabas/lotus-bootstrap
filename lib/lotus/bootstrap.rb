70eb1dc97a8cd96fd3761ea4bcfb983f8c0fc92b
Ciabas <a.wroblewski@binarapps.com>
2016-01-26 02:45:17 +0100

    [fix] name change lotus -> hanami

---
+++ lib/lotus/bootstrap.rb
@@ -0,0 +1,21 @@
+require "hanami/bootstrap/version"
+
+module Hanami
+  module Bootstrap
+    class << self
+      def load!
+        ::Hanami::Assets.sources << assets_path
+      end
+
+      def gem_path
+        @gem_path ||= File.expand_path '../..', File.dirname(__FILE__)
+      end
+
+      def assets_path
+        @assets_path ||= File.join gem_path, 'vendor', 'assets'
+      end
+    end
+  end
+end
+
+Hanami::Bootstrap.load!
